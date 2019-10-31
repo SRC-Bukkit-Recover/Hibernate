e210f57e7cd9199ca98258cdebcc50ebf2934486
HSGamer <huynhqtienvtag@gmail.com>
2019-10-31 23:11:46 +0700

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/main/java/com/woutwoot/hibernate/Main.java,5/a/5a69f9c5f88343dee69d29b47a45c0b6730e47cb
---
+++ src/main/java/com/woutwoot/hibernate/Main.java
@@ -0,0 +1,68 @@
+package com.woutwoot.hibernate;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class Main extends JavaPlugin {
+
+  private boolean enabled = true;
+  private BukkitTask task;
+
+  @Override
+  public void onDisable() {
+    if (task != null && !task.isCancelled()) {
+      task.cancel();
+    }
+  }
+
+  @Override
+  public boolean onCommand(final CommandSender sender, final Command command, final String label,
+      final String[] args) {
+    if (command.getName().equalsIgnoreCase("hibernate") && (sender.isOp() || sender
+        .hasPermission("hibernate.toggle"))) {
+      sender.sendMessage(
+          "[Hibernate] Hibernate is now " + (this.toggleEnabled() ? "enabled" : "disabled"));
+      return true;
+    }
+    return false;
+  }
+
+  private boolean toggleEnabled() {
+    this.enabled = !this.enabled;
+    return this.enabled;
+  }
+
+  @Override
+  public void onEnable() {
+    task = (new BukkitRunnable() {
+      boolean firstRun = true;
+
+      @Override
+      public void run() {
+        if (Bukkit.getServer().getOnlinePlayers().isEmpty() && Main.this.enabled) {
+          if (this.firstRun) {
+            for (final World w : Bukkit.getWorlds()) {
+              for (final Chunk c : w.getLoadedChunks()) {
+                c.unload(true);
+              }
+            }
+          }
+          try {
+            Thread.sleep(1000L);
+            this.firstRun = false;
+          } catch (Exception ignored) {
+            // IGNORED
+          }
+        } else {
+          this.firstRun = true;
+        }
+      }
+    }).runTaskTimer(this, 0, 1);
+  }
+}
